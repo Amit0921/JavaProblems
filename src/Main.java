8abd953425f7cca3a9a4156f6e8f0b618e74a55c
Amit Gupta <amitguptaa0921@gmail.com>
2025-06-12 11:57:36 +0530

    from stl system to git

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-//        MySingleton m1 = MySingleton.getInstance();
-//        MySingleton m2 = MySingleton.getInstance();
-//        MySingleton m3 = MySingleton.getInstance();
-        MySingleton m1 = new MySingleton();
-        MySingleton m2 = new MySingleton();
-        MySingleton m3 = new MySingleton();
+        MySingleton m1 = MySingleton.getInstance();
+        MySingleton m2 = MySingleton.getInstance();
+        MySingleton m3 = MySingleton.getInstance();
+//        MySingleton m1 = new MySingleton();
+//        MySingleton m2 = new MySingleton();
+//        MySingleton m3 = new MySingleton();
         m1.setData(1);
 
         System.out.println(m1.hashCode());
